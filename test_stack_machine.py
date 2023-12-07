5ea8db9caa48f7e92725f44675b066cab9276a9f
fllu760e <florian.lubitz@mailbox.tu-dresden.de>
2023-12-07 18:38:35 +0100

    t4 - test top and push

--- test_stack_machine.py
+++ test_stack_machine.py
@@ -16,6 +16,76 @@ class TestStackMachine(unittest.TestCase):
         self.assertEqual(sm.overflow, False)
         self.assertEqual(sm.stack, [])
 
+    def test_top(self):
+        # Test when stack is empty
+        self.assertIsNone(self.sm.top())
+
+        # Test when stack has one element appended
+        self.sm.stack.append((0, 1, 0, 1, 0, 1, 0, 1, 0))
+        self.assertEqual(self.sm.top(), (0, 1, 0, 1, 0, 1, 0, 1, 0))
+
+        # Test when stack has multiple elements appended
+        self.sm.stack.append((0, 0, 0, 0, 0, 0, 0, 0, 0))
+        self.assertEqual(self.sm.top(), (0, 0, 0, 0, 0, 0, 0, 0, 0))
+
+        self.sm.stack = [
+            self._intToByteTuple(255),
+            (1, 1, 1, 1, 1, 1, 1, 0),
+            (1, 1, 1, 1, 1, 1, 0, 0),
+            (1, 1, 1, 1, 1, 0, 0, 0),
+        ]
+        self.assertEqual(self.sm.top(), (1, 1, 1, 1, 1, 0, 0, 0))
+
+    def test_push(self):
+        # Push an integer
+        self.sm.stack = []
+        self.sm._push(1)
+        self.assertEqual(self.sm.stack, [self._intToByteTuple(1)])
+
+        # Push a tuple
+        self.sm.stack = []
+        self.sm._push(
+            (1, 1, 1, 1, 1, 0, 0, 0),
+        )
+        self.assertEqual(
+            self.sm.stack,
+            [
+                (1, 1, 1, 1, 1, 0, 0, 0),
+            ],
+        )
+
+        # Push a character
+        self.sm.stack = []
+        self.sm._push("A")
+        self.assertEqual(self.sm.stack, ["A"])
+
+        # Push multiple tuples and integers
+        self.sm.stack = []
+        self.sm._push(1)
+        self.sm._push(self._intToByteTuple(2))
+        self.sm._push("B")
+        self.sm._push((1, 1, 1, 1, 1, 0, 0, 0))
+        self.sm._push(self._intToByteTuple(255))
+        self.assertEqual(
+            self.sm.stack,
+            [
+                self._intToByteTuple(1),
+                self._intToByteTuple(2),
+                "B",
+                (1, 1, 1, 1, 1, 0, 0, 0),
+                self._intToByteTuple(255),
+            ],
+        )
+
+    def _inputValueToTuple(self, value: int) -> tuple:
+        return tuple(int(bit) for bit in bin(value)[2:].zfill(6))
+
+    def _intToByteTuple(self, value: int) -> tuple:
+        return tuple(int(bit) for bit in bin(value)[2:].zfill(8))
+
+    def _tupleToByteInt(self, value: tuple) -> int:
+        return int("".join([str(i) for i in value]), 2)
+
     def test_parse_byte(self):
         pass
 if __name__ == "__main__":
