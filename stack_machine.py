375785ac85a07017e00683adc3630c50aa8b0775
Florian Lubitz <florian.lubitz@mailbox.tu-dresden.de>
2023-12-07 11:56:36 +0100

    a4 - start with stack machine

--- stack_machine.py
+++ stack_machine.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python3
 
-from enum import IntEnum
+from enum import IntEnum, Enum
 from typing import List, Tuple, Union
 from ctypes import c_ubyte
 
 
 # IMPORTANT NOTE: DO NOT IMPORT THE ev3dev.ev3 MODULE IN THIS FILE
 
+
+class Instruction(Enum):
+    STP = 0b010000
+    DUP = 0b010001
+    DEL = 0b010010
+    SWP = 0b010011
+    ADD = 0b010100
+    SUB = 0b010101
+    MUL = 0b010110
+    DIV = 0b010111
+    EXP = 0b011000
+    MOD = 0b011001
+    SHL = 0b011010
+    SHR = 0b011011
+    HEX = 0b011100
+    FAC = 0b011101
+    NOT = 0b011110
+    XOR = 0b011111
+    NOP = None
+    SPEAK = 0b100001
+
+    def __str__(self):
+        return self.name
+
+
 class SMState(IntEnum):
     """
     Return codes for the stack machine
     """
+
     RUNNING = 1
     STOPPED = 0
     ERROR = -1
@@ -28,6 +54,29 @@ class StackMachine:
         self.overflow = False
         self.stack = []
 
+    def parse_byte(self, code_word: Tuple[int, ...]) -> int or Instruction or str:
+        # TODO: tuple of bin to int
+        byte = int("".join([str(i) for i in code_word]), 2)
+        if 0 <= byte <= 15:
+            # Is a number
+            return byte
+        elif 16 <= byte <= 31:
+            # Is an instruction
+            return Instruction(byte)
+        elif 32 <= byte <= 35:
+            # Is a special case
+            if byte == 33:
+                return Instruction.SPEAK
+            elif byte == 34:
+                return " "
+            else:
+                return Instruction.NOP
+        elif 36 <= byte <= 61:
+            # Is a letter
+            return chr(ord("A") + byte - 36)
+        else:
+            return Instruction.NOP
+
     def do(self, code_word: Tuple[int, ...]) -> SMState:
         """
         Processes the entered code word by either executing the instruction or pushing the operand on the stack.
@@ -38,7 +87,26 @@ class StackMachine:
             SMState: Current state of the stack machine
         """
         # REPLACE "pass" WITH YOUR IMPLEMENTATION
-        pass
+        # Clear stack and overflow flag
+        word = self.parse_byte(code_word)
+        print(
+            "Instruction is:",
+            word,
+            "Stack is:",
+            self.stack,
+            "Overflow flag is:",
+            self.overflow_flag,
+        )
+        if isinstance(word, Instruction):
+            print("\tRun instruction", word)
+            if self.run_instruction(word) == 1:
+                print("Final stack is: ", self.stack)
+                return
+        else:
+            print("\tPushing", word)
+            self.stack.append(word)
+            self.overflow_flag = False
+        print("\tStack after instruction: ", self.stack)
 
     def top(self) -> Union[None, str, Tuple[int, int, int, int, int, int, int, int]]:
         """
@@ -50,3 +118,96 @@ class StackMachine:
 
         # REPLACE "pass" WITH YOUR IMPLEMENTATION
         pass
+
+    def push(self, value: int or Tuple[int, int, int, int, int, int, int, int]) -> None:
+        """
+        Pushes a value on the stack.
+        """
+        if isinstance(value, int):
+            self.stack.append(tuple(int(bit) for bit in bin(value)[2:]))
+        elif isinstance(value, tuple):
+            self.stack.append(value)
+        pass
+
+    def get_operands_from_stack(self, n=2) -> int:
+        if len(self.stack) < n:
+            raise ValueError("Stack underflow")
+        else:
+            return tuple(
+                int("".join([str(i) for i in self.top()]), 2) for _ in range(n)
+            )
+
+    def run_instruction(self, instr: Instruction):
+        MAX_INT = 255
+        if instr == Instruction.STP:
+            return 1
+        elif instr == Instruction.DUP:
+            ops = self.get_operands_from_stack(1)
+            self.stack.append(ops[0])
+            self.stack.append(ops[0])
+        elif instr == Instruction.DEL:
+            print("Do DEL")
+        elif instr == Instruction.SWP:
+            print("Do SWP")
+        elif instr == Instruction.ADD:
+            ops = self.get_operands_from_stack()
+            result = ops[1] + ops[0]
+            if result > MAX_INT:
+                result = result % (MAX_INT + 1)
+                self.overflow_flag = True
+            self.stack.append(result)
+        elif instr == Instruction.SUB:
+            ops = self.get_operands_from_stack()
+            result = ops[1] - ops[0]
+            if result < 0:
+                result = (MAX_INT + 1) + result
+                self.overflow_flag = True
+            self.stack.append(result)
+        elif instr == Instruction.MUL:
+            ops = self.get_operands_from_stack()
+            result = ops[1] * ops[0]
+
+            if result > MAX_INT:
+                result = result % (MAX_INT + 1)
+                self.overflow_flag = True
+            self.stack.append(result)
+        elif instr == Instruction.DIV:
+            ops = self.get_operands_from_stack()
+            self.overflow_flag = False
+            self.stack.append(ops[1] // ops[0])
+        elif instr == Instruction.EXP:
+            ops = self.get_operands_from_stack()
+            result = ops[1] ** ops[0]
+
+            if result > MAX_INT:
+                result = result % (MAX_INT + 1)
+                self.overflow_flag = True
+            self.stack.append(result)
+        elif instr == Instruction.MOD:
+            self.overflow_flag = False
+            ops = self.get_operands_from_stack()
+            result = ops[1] % ops[0]
+            self.stack.append(result)
+        elif instr == Instruction.SHL:
+            print("Do SHL")
+        elif instr == Instruction.SHR:
+            self.overflow_flag = False
+            ops = self.get_operands_from_stack()
+            result = ops[1] >> ops[0]
+            self.stack.append(result)
+        elif instr == Instruction.HEX:
+            print("Do HEX")
+        elif instr == Instruction.FAC:
+            print("Do FAC")
+        elif instr == Instruction.NOT:
+            print("Do NOT")
+        elif instr == Instruction.XOR:
+            self.overflow_flag = False
+            ops = self.get_operands_from_stack()
+            result = ops[1] ^ ops[0]
+            self.stack.append(result)
+        elif instr == Instruction.NOP:
+            print("Do NOP")
+        elif instr == Instruction.SPEAK:
+            print("Do SPEAK")
+        return 0
